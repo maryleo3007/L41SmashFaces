a06d8c1742e671ffccd5e3bf2c468dd317976921
Janine Vega <osmary.vega@gmail.com>
2017-06-09 19:25:39 -0500

    add

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#mySelect").on('click',function () {
+      var selectCode = $(this).val();
+      console.log(selectCode);
+    });
+});
+
+var state = {
+  codermex:mexico,
+  coderper:peru,
+  select: null
+}
