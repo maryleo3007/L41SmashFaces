8673a142b42f6c03c6a46cd8ff89f8ebe0196014
Misheca <Misheca>
2017-06-12 01:01:44 -0500

    add interval

--- js/app.js
+++ js/app.js
@@ -2,47 +2,77 @@ $(function() {
     var contentImage = $('#contentImage');
     var name="";
     var countTry = 1;
+    var selectCode = "";
+    var sede, srcSede, imgrandom;
     $("#mySelect").on('change',function () {
-      var selectCode = $(this).val();
+      selectCode = $(this).val();
 
-      var imgrandom = getRandomArbitrary(1,state.codermex.length);
       var role = 1;
-
-      state.codermex.forEach(function (coder) {
-        var img = $('<img id="imgCoder" src="" alt="" class="img-responsive">');
-        var srcimg = './assets/img/mexico/'+coder.image;
-        img.attr("src",srcimg);
-        img.attr("role-img",role)
-        if(imgrandom == role){
-          name = coder.name;
-          contentImage.append(img);
+      $('#contentImage img').css({"display":"none"});
+      if(selectCode == 1){
+        srcSede= './assets/img/mexico/';
+        sede =  state.codermex;
+        imgrandom = getRandomArbitrary(1,sede.length);
+        getCoder(role,imgrandom,sede,srcSede);
+      }else if (selectCode == 2) {
+          srcSede= './assets/img/peru/';
+          sede = state.coderper;
+          imgrandom = getRandomArbitrary(1,sede.length);
+          getCoder(role,imgrandom,sede,srcSede);
         }
-        role++;
-      });
     });
+
     $("#btnComprobar").on('click',function () {
       var input = $("#inputname");
+      var role = 1;
+      var points = 0;
       countTry++;
+
       if(countTry <= 5){
-        if(input.val() == name){
-            alert("Acertaste");
+        if(input.val().toLowerCase() == name.toLowerCase()){
+            alert("Excelente, Acertaste");
+            input.val("");
+            points += 5;
+            $('#showMessage').html('<strong>'+points+'</strong>') ;
+            setTimeout(function () {
+              $('#contentImage img').css({"display":"none"});
+              imgrandom = getRandomArbitrary(1,sede.length);
+              getCoder(role,imgrandom,sede,srcSede);
+            },2000);
         }else{
-            alert("vuekve a intetntarlo");
+            alert("Sigue intentando");
+            points--;
         }
       }else{
-        alert("volver a pintar");
-      }
+          alert("hola");
+          $('#contentImage img').css({"display":"none"});
+          imgrandom = getRandomArbitrary(1,sede.length);
+          getCoder(role,imgrandom,sede,srcSede);
 
+      }
     });
 
-
+var getCoder = function (role,imgrandom,sede,srcSede) {
+  sede.forEach(function (coder) {
+    var img = $('<img id="imgCoder" src="" alt="" class="img-responsive">');
+    var srcimg = srcSede+coder.image;
+    img.attr("src",srcimg);
+    img.attr("role-img",role)
+    if(imgrandom == role){
+      name = coder.name;
+      contentImage.append(img);
+    }
+    role++;
+  });
+}
 });
 
-function getRandomArbitrary(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
 var state = {
   codermex:mexico,
   coderper:peru,
   select: null
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
